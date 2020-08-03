69102eca4927afecf0f7e5203c0fd216315785b9
lohitha-anumala <68274764+lohitha-anumala@users.noreply.github.com>
2020-08-03 10:00:25 +0530

    Add files via upload

---
+++ c#Assignments/Assignment1In/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+
+ using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Assignment1ln
+
+{
+    abstract class Quadrilateral
+    {
+
+        public int length;
+        public Quadrilateral()
+        {
+
+        }
+        public Quadrilateral(int _length)
+        {
+            length = _length;
+        }
+
+        abstract public int Area();
+        //abstract public void Display();
+
+    }
+    class Square : Quadrilateral
+    {
+
+        public Square()
+        {
+
+        }
+        public Square(int length) : base(length)
+        {
+
+        }
+        public override int Area()
+        {
+            int area = length * length;
+            return area;
+        }
+
+
+
+    }
+    class Rectangle : Quadrilateral
+    {
+        int breadth;
+
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int length, int _breath) : base(length)
+        {
+            breadth = _breath;
+
+
+
+        }
+        public override int Area()
+        {
+            return (breadth * length);
+        }
+
+    }
+
+    class Program
+
+
+    {
+        static void Main(string[] args)
+        {
+            Square s = new Square(4);
+            Console.WriteLine("Square Area : " + s.Area());
+
+            Quadrilateral q = new Rectangle(6, 5);
+            Console.WriteLine("Rectangle Area : " + q.Area());
+
+
+            if (s.Area() > q.Area())
+            {
+                Console.WriteLine("Square area is greater than Rectangle area");
+            }
+            else
+            {
+                Console.WriteLine("Rectangle area is greater than Square area");
+            }
+
+
+            Console.Read();
+
+        }
+    } 
+}
+
---
+++ c#Assignments/Assignment2/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+
+    public class Game
+    {
+        public int noOfPlayers;
+        public string country;
+        public string CurrentYear = DateTime.UtcNow.ToString("yyyy");
+        public Game()
+        {
+
+        }
+        public Game(int _noOfPlayers, string _country)
+        {
+            noOfPlayers = _noOfPlayers;
+            country = _country;
+        }
+        public void WorldCup(string country,int CurrentYear)
+        {
+            Console.WriteLine(" {0} have won WorldCup-{1}", country,CurrentYear);
+        }
+    }
+    public class Criket : Game
+    {
+        public string coachName;
+        public Criket()
+        {
+
+        }
+        public Criket(int _noOfPlayers, string _country, string _coach) : base(_noOfPlayers, _country)
+        {
+
+        }
+
+    }
+    class FootBall : Game
+    {
+        public string LeagueName;
+
+        public FootBall()
+        {
+
+        }
+        public FootBall(int _noOfPlayers, string _country, string _LeagueName) : base(_noOfPlayers, _country)
+        {
+            LeagueName = _LeagueName;
+        }
+    }
+    class ShowGameDetails
+    {
+        public void ShowCricketDetails(string country,int noOfPlayers,string Coach )
+        {
+            //Console.WriteLine("---------------CricketDetails----------------");
+            Console.WriteLine("Country: {0} No of Players :{1} Coach :{2} ",country,noOfPlayers,Coach);
+        }
+        public void ShowFootBallDetails(string country, int noOfPlayers, string Coach)
+        {
+           //Console.WriteLine("====================FootBall Details===============================");
+            Console.WriteLine("Country: {0} No of Players :{1} Coach :{2} ", country, noOfPlayers, Coach);
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Criket India = new Criket();
+            Criket Australia = new Criket();
+            FootBall Spain = new FootBall();
+            FootBall England = new FootBall();
+            ShowGameDetails show=new ShowGameDetails();
+            Console.WriteLine("===========Cricket Details======================");
+            show.ShowCricketDetails("India",11,"SAchin");
+            show.ShowCricketDetails("Australia", 11, "SAchin");
+            Console.WriteLine("===============Foot Ball Details=================");
+
+            show.ShowFootBallDetails("Australia",8,"Dhoni");
+            show.ShowFootBallDetails("Spain", 8, "Dhoni");
+
+            Game G = new Game();
+            Console.WriteLine("                               "            );
+
+            
+            Australia.WorldCup("Australia",2007);
+            Spain.WorldCup("Spain",20007);
+
+
+
+
+
+
+
+
+        }
+    }
+}
